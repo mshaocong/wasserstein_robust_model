f73a9484cd140b956a41709f1de5c61111aebb99
sc_ma <46854477+mshaocong@users.noreply.github.com>
2021-12-23 15:41:00 +0800

    Edit codes.

--- train.py
+++ train.py
@@ -2,49 +2,53 @@ from model.net import Net
 import torch
 import torchvision
 import numpy as np
-from wrm import WassersteinRobustModel
+from wrm import WRM
 from utils import *
 import time
+import argparse
+
 torch.manual_seed(2)
 np.random.seed(3)
 
-NUM_EPOCHS = 200
+NUM_EPOCHS = 100
 
-# TODO: (1) Move all hyper-parameter setting to __init__ function.
-#       (2) Automatically normalizing data.
-#       (3) Add more functions.
+# TODO: (1) Implement the MomentumWRM
 #       (4) Conduct a hyper-parameter exploration. Draw some plots. Write a technical report.
 
 
 def main():
     net = Net()
     transform = torchvision.transforms.Compose([
-        # transforms.Grayscale(3),
         torchvision.transforms.ToTensor()
     ])
     trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=60000,
+                                              shuffle=True, num_workers=0)
+    trainset = next(iter(trainloader))
 
     testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=10000,
                                              shuffle=False, num_workers=0)
 
-    # Generate the indices
-    batch_size = 1000
 
     # Optimizer parameters
-    opt_kwargs = {"lr" : 0.001}
+    batch_size = 1000
+    lr_ascent = 10e-3
+    lr_descent = 10e-3
+    num_ascent_steps = 1
 
-    model = WassersteinRobustModel(net, trainset, optimizer=torch.optim.SGD, **opt_kwargs)
+    model = WRM(net, trainset[0], trainset[1], attacker_type="L2",
+                                   lr_descent=lr_descent, lr_ascent=lr_ascent)
     accuracy = []
     for i in range(NUM_EPOCHS):
-        sample_indeces_list = get_indices(batch_size, len(trainset.targets))
+        sample_indeces_list = get_indices(batch_size, len(trainset[0]))
         for index in sample_indeces_list:
-            model.update(index, lr_ascent=0.001)
+            model.update(index, num_ascent_steps=num_ascent_steps)
 
         num_correct = 0
         num_total = 0
         for data in testloader:
-            _num_correct, _num_total = model.evaluate(data)
+            _num_correct, _num_total = model.evaluate(data[0], data[1])
             num_correct,  num_total = num_correct + _num_correct, num_total + _num_total
         accuracy.append(num_correct/num_total)
         print(i, accuracy[-1])
--- wrm.py
+++ wrm.py
@@ -9,37 +9,42 @@ import time
 # Check if GPU is corrected set up
 if torch.cuda.is_available():
     DEVICE = torch.device("cuda:0")
-    # torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    # dtype = torch.cuda.FloatTensor
 else:
     raise Exception("GPU is not set up!")
 
 
-class WassersteinRobustModel:
-    def __init__(self, net, dataset, optimizer, gamma=1.3, **kwargs):
+class WRM:
+    def __init__(self, net, data, labels, attacker_type="L2", lr_descent=10e-3, lr_ascent=10e-3, gamma=1.3):
         self.net = net
         self.net.to(DEVICE)
 
-        self.dataset = dataset
-        gen_dataset =  np.random.standard_normal(size = dataset.data.shape) + np.copy(dataset.data/255.0)
-        gen_dataset = np.expand_dims(gen_dataset, axis=1)
+        self.data = data
+        self.targets = labels
+        gen_dataset =  np.random.standard_normal(size = data.shape) + np.copy(data)
         self.gen_dataset = gen_dataset
 
         self.gamma = gamma
-        self.optimizer_descent = optimizer(self.net.parameters(), **kwargs)
+        self.optim_descent = torch.optim.SGD(self.net.parameters(), lr=lr_descent)
 
-    def update(self, indices, num_ascent_steps=1, lr_ascent=0.001):
+        self.lr_descent = lr_descent
+        self.lr_ascent = lr_ascent
+        self.attacker_type = attacker_type
+
+
+    def update(self, indices, num_ascent_steps=1):
+        lr_ascent = self.lr_ascent
+
         net = self.net
         net.to(DEVICE)
 
-        optimizer_descent = self.optimizer_descent
-        optimizer_descent.zero_grad()
+        optim_descent = self.optim_descent
+        optim_descent.zero_grad()
 
-        data = self.dataset.data[indices]
-        data = data.unsqueeze(1)
-        data = data/255.0
+        data = self.data[indices]
+        # data = data.unsqueeze(1)
+        # data = data/255.0
         data = data.float().to(DEVICE)
-        target = self.dataset.targets[indices].to(DEVICE)
+        target = self.targets[indices].to(DEVICE)
 
         # Stage 1 - One step gradient descent. Freeze the adversarial data `y`; do descent on net params.
         gen_data = torch.from_numpy(self.gen_dataset[indices]).float().to(DEVICE)
@@ -49,7 +54,7 @@ class WassersteinRobustModel:
         output = net(gen_data)
         loss_descent = F.nll_loss(output, target)
         loss_descent.backward()
-        optimizer_descent.step()
+        optim_descent.step()
 
         # Stage 2 - Multiple steps gradient ascent. Freeze net params; do ascent on the adversarial data `y`
         gen_data = torch.from_numpy(self.gen_dataset[indices]).float().to(DEVICE)
@@ -57,18 +62,41 @@ class WassersteinRobustModel:
         for param in net.parameters():
             param.requires_grad = False
         for step in range(num_ascent_steps):
+            # when the attacker is not specified, WRM degenerates to normal classification task
+            if self.attacker_type is None:
+                break
+
             output = net(gen_data)
-            loss_ascent = F.nll_loss(output, target) - self.gamma * torch.sum((data - gen_data) ** 2)
+            if self.attacker_type == "L2":
+                # loss_ascent = F.nll_loss(output, target) - self.gamma * torch.sum((data - gen_data) ** 2)
+                loss_ascent = F.nll_loss(output, target) - self.gamma * torch.linalg.vector_norm(data - gen_data, ord=2)
+            elif self.attacker_type == "L1":
+                loss_ascent = F.nll_loss(output, target) - self.gamma * torch.linalg.vector_norm(data - gen_data, ord=1)
+            else:
+                raise "Attacker type "+str(self.attacker_type)+" is not supported."
             loss_ascent.backward()
             gen_data.data = gen_data.data + lr_ascent * gen_data.grad.data
-            gen_data.data = l1proximal(gen_data.data, 0.0001)
+            # gen_data.data = l1proximal(gen_data.data, 0.0001)
             gen_data.grad.data.zero_()
         self.gen_dataset[indices] = np.copy(gen_data.data.cpu())
 
-    def evaluate(self, data):
-        images, labels = data
-        images = images.float().to(DEVICE)
+    def evaluate(self, data, labels):
+        data = data.float().to(DEVICE)
         labels = labels.float().to(DEVICE)
-        outputs = self.net(images)
+        outputs = self.net(data)
         _, predicted = torch.max(outputs.data, 1)
         return (predicted == labels).sum().item(), labels.size(0)
+
+
+class MomentumWRM(WRM):
+    def __init__(self, net, data, labels, attacker_type="L2", lr_descent=10e-3, lr_ascent=10e-3, momentum_descent=0.1,
+                 momentum_ascent=0.1, gamma=1.3):
+        super().__init__(net, data, labels, attacker_type, lr_descent, lr_ascent, gamma)
+        self.optim_descent = torch.optim.SGD(self.net.parameters(), lr=lr_descent, momentum=momentum_descent)
+        self.momentum_descent = momentum_descent
+        self.momentum_ascent = momentum_ascent
+
+        self.cache = np.copy(self.gen_dataset)
+
+    def update(self, indices, num_ascent_steps=1):
+        raise NotImplementedError
